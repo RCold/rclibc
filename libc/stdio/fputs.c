fc7d1c33ce8ef724676c5a12dfc4ad32a17f27dc
Yeuham Wang <rcold@rcold.name>
2021-11-09 18:05:57 +0800

    Fix some issues with fputs and fwrite

--- libc/stdio/fputs.c
+++ libc/stdio/fputs.c
@@ -1,8 +1,8 @@
 #include "stdio.h"
 
 int fputs(const char *s, FILE *fp) {
-    while (*s != '\0')
-        if (putc(*s++, fp) == EOF)
+    for (; *s != '\0'; s++)
+        if (putc(*s, fp) == EOF)
             return EOF;
     return 0;
 }
--- libc/stdio/fwrite.c
+++ libc/stdio/fwrite.c
@@ -3,8 +3,6 @@
 size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *fp) {
     const char *p = ptr;
     const char *end = p + size * nmemb;
-    while (p < end)
-        if (putc(*p++, fp) == EOF)
-            break;
+    for (; p < end && putc(*p, fp) != EOF; p++);
     return size == 0 ? nmemb : (p - (const char *) ptr) / size;
 }
