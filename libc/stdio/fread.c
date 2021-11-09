f7e6a572d75a2304cf7be4b6088b46fad566fba3
Yeuham Wang <rcold@rcold.name>
2021-11-09 18:27:29 +0800

    Optimize fread

--- libc/stdio/fread.c
+++ libc/stdio/fread.c
@@ -4,10 +4,7 @@ size_t fread(void *ptr, size_t size, size_t nmemb, FILE *fp) {
     int c;
     char *p = ptr;
     char *end = p + size * nmemb;
-    while (p < end) {
-        if ((c = getc(fp)) == EOF)
-            break;
+    while (p < end && (c = getc(fp)) != EOF)
         *p++ = (char) c;
-    }
     return size == 0 ? nmemb : (p - (char *) ptr) / size;
 }
