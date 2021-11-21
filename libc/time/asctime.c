57876e65db956be6dd8e86a538703182672c439b
Yeuham Wang <rcold@rcold.name>
2021-11-21 16:59:10 +0800

    Implement asctime

---
+++ libc/time/asctime.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+#include "time.h"
+
+static const char wday_name[7][3] = {
+        "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"
+};
+
+static const char mon_name[12][3] = {
+        "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+};
+
+static char buf[26];
+
+char *asctime(const struct tm *timeptr) {
+    sprintf(buf, "%.3s %.3s%3d %.2d:%.2d:%.2d %d\n", wday_name[timeptr->tm_wday], mon_name[timeptr->tm_mon],
+            timeptr->tm_mday, timeptr->tm_hour, timeptr->tm_min, timeptr->tm_sec, 1900 + timeptr->tm_year);
+    return buf;
+}
