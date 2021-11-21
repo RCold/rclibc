d013e55b4857eb995121ac6b0af71cd6651da7f2
Yeuham Wang <rcold@rcold.name>
2021-11-21 19:53:52 +0800

    Fix some issues with div and ldiv

--- libc/stdlib/ldiv.c
+++ libc/stdlib/ldiv.c
@@ -3,10 +3,15 @@
 ldiv_t ldiv(long int numer, long int denom) {
     ldiv_t ret;
     ret.quot = numer / denom;
-    ret.rem = numer - denom * ret.quot;
-    if (ret.quot < 0 && ret.rem > 0) {
-        ret.quot += 1;
-        ret.rem -= denom;
+    ret.rem = numer % denom;
+    if ((numer > 0 && ret.rem < 0) || (numer < 0 && ret.rem > 0)) {
+        if (ret.quot < 0) {
+            ret.quot++;
+            ret.rem -= denom;
+        } else if (ret.quot > 0) {
+            ret.quot--;
+            ret.rem += denom;
+        }
     }
     return ret;
 }
