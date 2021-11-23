51f1bfa4a20980b3d7c2d89d7933763e74246ece
Yeuham Wang <rcold@rcold.name>
2021-11-23 16:08:47 +0800

    Implement _tmpdir for Windows

--- libc/stdio/_tmpdir.c
+++ libc/stdio/_tmpdir.c
@@ -3,6 +3,15 @@
 #include "string.h"
 #include "sys/_stat.h"
 
+#ifdef _WIN32
+
+#include "stdlib.h"
+
+#define FILE_SEPARATOR   "\\"
+#else
+#define FILE_SEPARATOR   "/"
+#endif /* _WIN32 */
+
 #ifdef __APPLE__
 #define P_tmpdir    "/var/tmp/"
 #else
@@ -15,11 +24,15 @@ static int direxists(const char *dir) {
 }
 
 int _tmpdir(char *s, size_t size) {
+#ifdef _WIN32
+    char *dir = getenv("TMP");
+#else
     char *dir = P_tmpdir;
+#endif /* _WIN32 */
     size_t dlen = strlen(dir);
-    while (dlen > 1 && dir[dlen - 1] == '/')
+    while (dlen > 1 && (dir[dlen - 1] == '/' || dir[dlen - 1] == '\\'))
         dlen--;
     if (dlen + 2 > size || !direxists(dir) || access(dir, W_OK) != 0)
         return -1;
-    return sprintf(s, "%.*s/", (int) dlen, dir);
+    return sprintf(s, "%.*s" FILE_SEPARATOR, (int) dlen, dir);
 }
