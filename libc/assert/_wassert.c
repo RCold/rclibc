7a5b8d95e5689f4dec538618a628941fd448c21a
Yeuham Wang <rcold@rcold.name>
2021-11-24 18:30:31 +0800

    Optimize _wassert and asctime

--- libc/assert/_wassert.c
+++ libc/assert/_wassert.c
@@ -2,9 +2,7 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-static char *assert_str = "Assertion failed: %s, file %s, line %u\n";
-
 void _wassert(const char *expr, const char *file, unsigned int line) {
-    fprintf(stderr, assert_str, expr, file, line);
+    fprintf(stderr, "Assertion failed: %s, file %s, line %u\n", expr, file, line);
     abort();
 }
